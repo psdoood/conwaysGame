c4fe22f70515ba74ed3ebad228a9b6b1e30d590c
psdoood <aydeniswright@gmail.com>
2024-11-25 20:03:46 -0700

    Added unique id for each cell, and added alive neighbor counting function.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ size = 20;
 isRunning = false;
 mouseIsDown = false;
 
+//Changes the number by the range input as it is updated
 $("#grid-size").on("input", function(){
     size = $(this).val();
     $("#size-display").text(size);
@@ -12,13 +13,14 @@ function createGrid(){
     gridHtml = "";
     for(i = 0; i < size; i++){
         for(j = 0; j < size; j++){
-            gridHtml += "<div class='cell'></div>";
+            gridHtml += "<div class='cell' id='" + i + '-' + j + "'></div>";
         }
         gridHtml += "<br>";
     }
     $("#grid").html(gridHtml);
 }
 
+//The next section of code handles drawing on the grid
 $("#clear").click(function(){
     $(".cell").removeClass("alive");
 })
@@ -37,5 +39,22 @@ $("#grid").on("mousedown", ".cell", function(){
 $("#grid").on("mouseup", function(){
     mouseIsDown = false;
 });
+
+//Game logic section
+const dirs = [[1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1], [0,1]];
+function countDirs(row, col){
+    count = 0;
+    for([dRow, dCol] of dirs){
+        newRow = row + dRow;
+        newCol = col + dCol;
+        if(newRow < 0 || newRow >= size || newCol < 0 || newCol >= size){
+            continue;
+        }
+        if($("#" + newRow + "-" + newCol).hasClass("alive")){
+            count++;
+        }
+    }
+    return count;
+}
 
 createGrid();
